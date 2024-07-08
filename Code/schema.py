38250d1b447e660d1b7d17ddfac6160e51371ab9
Kaiwalya Deshpande <kaiwalya@live.in>
2024-07-08 21:06:53 +0530

    Using Vue3 without CLI

--- Code/schema.py
+++ Code/schema.py
@@ -1,8 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(250), nullable=False)
     last_name = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    type = db.Column(db.Integer, nullable=False)
--- Code/static/index.js
+++ Code/static/index.js
@@ -1,8 +1,67 @@
+var login_page = {
+	login: true,
+	login_details: {
+		username: "",
+		password: "",
+	},
+	register_details: {
+		first_name: "",
+		last_name: "",
+		password: "",
+		username: "",
+		email: "",
+		type: 0,
+	},
+};
 const app = {
 	data() {
-		return { greeting: "Hello from Vue!" };
+		return {
+			user: {},
+			loggedIn: false,
+			login_page: window.login_page,
+		};
+	},
+	methods: {
+		getAdminData: function () {
+			axios.get("http://127.0.0.1:5000/loggedin").then((response) => {
+				this.loggedIn = response.data;
+			});
+		},
+		loginHandler: function () {
+			console.log(login_page.login_details);
+			if (
+				login_page.login_details.username != "" &&
+				login_page.login_details.password != ""
+			) {
+				axios
+					.post("http://127.0.0.1:5000/login", this.login_page.login_details)
+					.then((response) => {
+						console.log(response);
+						if (response.data) {
+							this.getAdminData();
+							window.location.href = "http://127.0.0.1:5000";
+						}
+					});
+			}
+		},
+		logout: function () {
+			if (!this.loggedIn) {
+				alert("You are not Logged In!");
+			} else {
+				axios.get("https://127.0.0.1:5000/logout").then((response) => {
+					console.log(response);
+					if (response.data) {
+						this.loggedIn = false;
+						window.location.href = "http://127.0.0.1:5000/login";
+					}
+				});
+			}
+		},
+	},
+	created() {
+		this.getAdminData();
 	},
 	delimiters: ["[[", "]]"],
 };
 
-Vue.createApp(app).mount("#hello");
+Vue.createApp(app).mount("#app");
