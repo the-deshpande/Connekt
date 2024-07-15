fd3bc581e74e43c3f13ec0bac6516ce88a65203c
Kaiwalya Deshpande <kaiwalya@live.in>
2024-07-16 02:27:09 +0530

    Created well detailed serializations

--- Code/schema.py
+++ Code/schema.py
@@ -12,7 +12,7 @@ class User(db.Model):
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
     flagged = db.Column(db.Boolean, default=False)
-    type = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.Integer, nullable=False) # 0 = admin | 1 = influencer | 2 = sponsor
 
     @property
     def serialize(self):
@@ -22,21 +22,23 @@ class User(db.Model):
             "first_name" : self.first_name,
             "last_name" : self.last_name,
             "email" : self.email,
-            "type" : self.type
+            "flagged": self.flagged,
+            "type" : self.type,
+            "influencer": self.influencer.serialize if self.influencer is not None else None,
+            "sponsor": self.sponsor.serialize if self.sponsor is not None else None,
         }
+        
 
 class Influencer(db.Model):
     __tablename__ = 'influencers'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    contract = db.relationship('Contract', backref='influencer')
-    platform = db.Column(db.String(250), nullable=False)
+    platform = db.Column(db.String(250), nullable=False) # Youtube | TikTok | Instagram | Twitter
     reach = db.Column(db.Integer, nullable=False)
-    niche = db.Column(db.String(250), nullable=False)
+    niche = db.Column(db.String(250), nullable=False) # Healthcare | Gaming | Tech | Vlog
 
     @property
     def serialize(self):
-        """ Returns dictionary that is easily serializable """
         return {
             "id": self.id,
             "platform": self.platform,
@@ -48,14 +50,12 @@ class Sponsor(db.Model):
     __tablename__ = 'sponsors'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    campaign = db.relationship('Campaign', backref='sponsor')
     company = db.Column(db.String(250), nullable=False)
-    industry = db.Column(db.String(250), nullable=False)
+    industry = db.Column(db.String(250), nullable=False) # Healthcare | Gaming | Tech | Vlog
     budget = db.Column(db.Integer, nullable=False)
 
     @property
     def serialize(self):
-        """ Returns dictionary that is easily serializable """
         return {
             "id": self.id,
             "company": self.company,
@@ -67,7 +67,7 @@ class Campaign(db.Model):
     __tablename__ = 'campaigns'
     id = db.Column(db.Integer, primary_key=True)
     sponsor_id = db.Column(db.Integer, db.ForeignKey('sponsors.id'), nullable=False)
-    contract = db.relationship('Contract', backref='campaign')
+    sponsor = db.relationship('Sponsor', backref='campaign')
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250), nullable=False)
     start_date = db.Column(db.DateTime, nullable=False)
@@ -82,29 +82,35 @@ class Campaign(db.Model):
         """ Returns dictionary that is easily serializable """
         return {
             "id": self.id,
+            "sponsor": self.sponsor.user.serialize,
             "name": self.name,
             "description": self.description,
             "start_date": self.start_date.strftime("%d-%m-%Y"),
             "end_date": self.end_date.strftime("%d-%m-%y"),
             "public": self.public,
             "goals": self.goals,
-            "approved": self.approved
+            "approved": self.approved,
+            "flagged": self.flagged,
         }
 
 class Contract(db.Model):
     __tablename__ = 'contracts'
     id = db.Column(db.Integer, primary_key=True)
     campaign_id = db.Column(db.Integer, db.ForeignKey('campaigns.id'), nullable=False)
+    campaign = db.relationship('Campaign', backref='contract')
     influencer_id = db.Column(db.Integer, db.ForeignKey('influencers.id'), nullable=False)
+    influencer = db.relationship('Influencer', backref='contract')
     requirements = db.Column(db.String(250), nullable=False)
     payment_amount = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.Integer, default=0)
+    status = db.Column(db.Integer, default=0) # 0 = rejected | 1 = influencer | 2 = sponsor | 3 = approved
 
     @property
     def serialize(self):
         """ Returns dictionary that is easily serializable """
         return {
             "id": self.id,
+            "campaign": self.campaign.serialize,
+            "influencer": self.influencer.user.serialize,
             "requirements": self.requirements,
             "payment_amount": self.payment_amount,
             "status": self.status
